334ed2e2191ff4e0e0572cd50aa06fb35c110085
Andrew Stiegmann (stieg) <andrew.stiegmann@gmail.com>
2010-12-20 00:33:21 -0800

    Added code for key pressing and started functiond define for move ops.  Also added more USB logic

--- the_football.c
+++ the_football.c
@@ -22,6 +22,16 @@
 #include <glib/gprintf.h>
 #include <usb.h>
 
+/* These will change eventually */
+#define FB_KEY_DOWN   'k'
+#define FB_KEY_FIRE   'f'
+#define FB_KEY_LEFT   'j'
+#define FB_KEY_QUIT   'q'
+#define FB_KEY_RESET  'r'
+#define FB_KEY_RIGHT  'l'
+#define FB_KEY_UP     'i'
+
+
 /*
  * T:  Bus=03 Lev=01 Prnt=01 Port=04 Cnt=03 Dev#=  7 Spd=1.5 MxCh= 0
  * D:  Ver= 1.10 Cls=00(>ifc ) Sub=00 Prot=00 MxPS= 8 #Cfgs=  1
@@ -74,23 +84,65 @@ find_rocket_baby ()
 } /* find_rocket_baby */
 
 
+//#define MOVE_FUNC(name, code)
+/* static void */
+/* move_left(struct usb_device_handler *h) */
+/* { */
+/*    usb_control_msg(h, 0x21, 0x9, 0x200, index, msg, 8, 1000); */
+/* } */
+
+/* MOVE_FUNC(up, 1) */
+/* MOVE_FUNC(down, 2) */
+/* MOVE_FUNC(left, 4) */
+/* MOVE_FUNC(right, 8) */
+
+
 int
 main()
 {
-   struct usb_device *h;
-   WINDOW *win;
+   struct usb_device *dev;
+   struct usb_dev_handle *devHandle;
+   int status;
+   gboolean quit;
+
+   /* Initialize curses and the usb lib */
+   initscr();
+   cbreak();
    usb_init();
 
-   h = find_rocket_baby();
-   /* if (h == NULL) */
-   /*    g_error("Rocket Baby device not found\n"); */
+   dev = find_rocket_baby();
+   if (dev == NULL)
+      g_error("Rocket Baby device not found\n");
 
-   win = initscr();
-   cbreak();
-   while(1) {
+   devHandle = usb_open(dev);
+   g_assert(devHandle);
+
+   status = usb_claim_interface(devHandle, 0);
+   if (status!= 0)
+      g_error("Failed to claim device\n");
+
+   /* And do the loop until we quit */
+   for(quit = FALSE; !quit;) {
       int c = getch();
-      g_printf("Got char %d\n", c);
-      fflush(NULL);
+
+      switch (c) {
+      case FB_KEY_UP:
+         break;
+      case FB_KEY_DOWN:
+         break;
+      case FB_KEY_LEFT:
+         break;
+      case FB_KEY_RIGHT:
+         break;
+      case FB_KEY_FIRE:
+         break;
+      case FB_KEY_RESET:
+         break;
+      case FB_KEY_QUIT:
+         quit = TRUE;
+      default:
+         break;
+      }
    }
 
    return 0;
